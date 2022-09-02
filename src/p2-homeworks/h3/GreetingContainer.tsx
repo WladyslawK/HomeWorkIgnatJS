346d2d2d223cbff642971e4ac6271d70c21b5ef4
WladyslawK <vladkurr7890@gmail.com>
2022-09-02 07:49:54 +0200

    Disappear error message when continue typing

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -18,6 +18,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
+        setError("")
     }
     const addUser = () => {
         if(name.trim()!==""){
